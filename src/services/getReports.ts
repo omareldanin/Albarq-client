82aa884316a0951dbf94f19de5499e113ad971af
omar mohamed <omer.eldanin@gmail.com>
2024-12-26 14:55:33 +0200

    edit report confirm column

--- src/services/getReports.ts
+++ src/services/getReports.ts
@@ -16,7 +16,7 @@ export interface Report {
     createdAt: string;
     companyNet: number;
     updatedAt: string;
-    confirm: boolean;
+    confirmed: boolean;
     companyReport: {
         reportNumber: string;
         company: {
